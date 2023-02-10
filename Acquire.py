1e312b392948a841e40b9813bf313634838ef73a
Jacob Panyathong <jacobpanyathong1@gmail.com>
2023-02-10 16:58:17 -0600

    Acquire file

---
+++ Acquire.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+import pandas as pd
+
+def get_city_df():
+    city_df = pd.read_csv('GlobalLandTemperaturesByCity.csv')
+    return city_df
+
+
+# In[ ]:
+
+
+
+
